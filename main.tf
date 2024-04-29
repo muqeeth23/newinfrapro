f638aeaf157c83c986b6777e161ab2383c57bf9d
muqeeth_md <mohammad.muqeeth@c360soft.ai>
2024-04-29 16:28:13 +0530

    added subnet 2 code

--- main.tf
+++ main.tf
@@ -16,3 +16,12 @@ resource "aws_subnet" "main" {
     Name = "Main"
   }
 }
+
+resource "aws_subnet" "main2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24"
+
+  tags = {
+    Name = "Main2"
+  }
+}
